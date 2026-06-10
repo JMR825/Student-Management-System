2e195e8f23f1962dc5f12938f59db1c6f9cb1a61
JMR825 <mdesktop262@gmail.com>
2026-06-10 22:24:21 +0530

    - modified addStudent() and display()

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<fstream>
 #include<filesystem>
 #include<vector>
+#include <sstream>
 struct Student
 {
     int id;
@@ -13,7 +14,6 @@ struct Student
 
 void addStudent()
 {
-    
     Student s;
     std::cout<<"Enter ID: ";
     std::cin>>s.id;
@@ -31,6 +31,15 @@ void addStudent()
     std::cout<<"name: "<<s.name<<std::endl;
     std::cout<<"age: "<<s.age<<std::endl;
     std::cout<<"course: "<<s.course<<std::endl;
+    std::ofstream file;
+    file.open("students.txt",std::ios::app);
+    if(!file.is_open())
+    {
+        std::cout<<"File did not open"<<std::endl;
+        return;
+    }
+    file << s.id << "|" << s.name << "|" << s.age << "|" << s.course << std::endl;
+    file.close();
 }
 void updateStudent()
 {
@@ -39,10 +48,61 @@ void updateStudent()
 void searchStudent()
 {
     std::cout<<"search Selected"<<std::endl;
+    std::string str;
+    std::cout<<"Enter id: ";
+    std::cin>>str;
+    std::ifstream file("students.txt");
+    std::string line;
+    if (!file.is_open())
+{
+    std::cout << "No records found" << std::endl;
+    return;
+}
+while(std::getline(file,line))
+{
+    std::stringstream ss(line);
+    std::string idStr,nameStr,ageStr,courseStr;
+    std::getline(ss,idStr,'|');
+    std::getline(ss,nameStr,'|');
+    std::getline(ss,ageStr,'|');
+    std::getline(ss,courseStr);
+    if(str==idStr)
+    {
+    std::cout<<idStr<<std::endl;
+    std::cout<<nameStr<<std::endl;
+    std::cout<<ageStr<<std::endl;
+    std::cout<<courseStr<<std::endl;
+}
+
+}
 }
 void display()
 {
     std::cout<<"display Selected"<<std::endl;
+    std::ifstream file("students.txt");
+    std::string line;
+    if (!file.is_open())
+{
+    std::cout << "No records found" << std::endl;
+    return;
+}
+    while(std::getline(file,line))
+    {
+    std::stringstream ss(line);
+    std::string idStr, nameStr, ageStr, courseStr;
+    
+    std::getline(ss, idStr, '|');
+    std::getline(ss, nameStr, '|');
+    std::getline(ss, ageStr, '|');
+    std::getline(ss, courseStr);
+    
+    std::cout << "ID: " << idStr << std::endl;
+    std::cout << "Name: " << nameStr << std::endl;
+    std::cout << "Age: " << ageStr << std::endl;
+    std::cout << "Course: " << courseStr << std::endl;
+    std::cout << std::endl;
+    }
+    file.close();
 }
 void deleteStudent()
 {
@@ -56,7 +116,6 @@ void menu()
 int main()
 {
     int choice;
-    
     while(true)
     {
         std::cout<<"----MENU----"<<std::endl;
